fbaf841770be3bbaddde30d2cda45f6b2abec4bd
Jasleen <jasleensekhon867@gmail.com>
2025-04-04 23:51:14 -0700

    Added tables.js file

---
+++ tables.js
@@ -0,0 +1,5 @@
+// STEP 11: Call to tablesorter script here
+//grab the table 
+const consolesTable=document.querySelector("table");
+//enable tablesort.js on the table
+let myTable=new Tablesort(consolesTable);
